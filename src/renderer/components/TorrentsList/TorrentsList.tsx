57f0918b0d05322564dc414586afc258753546e5
Brandon Pereira <brandonpereiras@gmail.com>
2022-07-22 18:12:31 -0600

    remove extra styles

--- src/renderer/components/TorrentsList/TorrentsList.tsx
+++ src/renderer/components/TorrentsList/TorrentsList.tsx
@@ -1,12 +1,11 @@
 import useTorrents from 'renderer/hooks/useTorrents';
-import styles from './TorrentList.module.scss';
 import Torrent from '../Torrent/Torrent';
 
 function TorrentsList() {
   const { torrents, loading } = useTorrents();
 
   return (
-    <div className={styles.container}>
+    <div>
       {loading && <>Loading</>}
       {torrents?.map((t) => (
         // eslint-disable-next-line react/jsx-props-no-spreading
